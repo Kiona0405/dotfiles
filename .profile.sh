a805bee1cff0337503ecf2c97137b90c1ed1482c
Kiona1018 <kiona1018@gmail.com>
2021-02-06 12:56:41 +0900

    add pyenv path

--- .profile.sh
+++ .profile.sh
@@ -1,8 +1,13 @@
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
 alias vim='nvim'
 alias gita='git add'
 alias gitd='git diff'
 alias gitchk='git checkout'
 alias gits='git status'
+
+
 gitcmt () {
     message=$1
     git commit -m ${message}
