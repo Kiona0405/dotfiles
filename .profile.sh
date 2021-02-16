08ccb3e5f5e379ab179da8889607ec95b9caa98a
Kiona1018 <kiona1018@gmail.com>
2021-02-16 23:24:58 +0900

    remove useless functions

--- .profile.sh
+++ .profile.sh
@@ -6,32 +6,3 @@ alias gita='git add'
 alias gitd='git diff'
 alias gitchk='git checkout'
 alias gits='git status'
-
-
-gitcmt () {
-    message=$1
-    git commit -m ${message}
-}
-alias dstamp='date +%Y%m%d'
-
-
-exe_cpp () {
-    name=$1
-    g++ ${name}.cpp -g -o ${name} -std=c++14 && ./${name}
-}
-
-# iTerm2 tab titles
-export DISABLE_AUTO_TITLE="true"
-unset PROMPT_COMMAND
-function title {
-    if [ "$1" ]
-    then
-        echo -ne "\033]0;${*}\007"
-    fi
-}
-
-alerm () {
-    sleep_time=$1
-    audio=/Users/Naoki/Projects/diary/audio/old_school_bell.mp3
-    sleep $sleep_time; afplay $audio 
-}
