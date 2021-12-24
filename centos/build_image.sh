87bb9f420c02257af7532a656873613a918409cf
Kiona0405 <selfhelp.nt@gmail.com>
2021-12-25 00:37:03 +0900

    complete centos docker

--- centos/build_image.sh
+++ centos/build_image.sh
@@ -6,4 +6,10 @@ cd $here
 
 source env.sh
 
-docker build --tag=$IMAGE .
+GIT_USER=$(git config -l | grep user.name | cut -d = -f 2)
+GIT_MAIL=$(git config -l | grep user.email| cut -d = -f 2)
+
+docker build\
+    --build-arg GIT_USER=GIT_USER\
+    --build-arg GIT_MAIL=GIT_MAIL\
+    --tag=$IMAGE .
